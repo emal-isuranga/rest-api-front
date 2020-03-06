f65313edee782564cd0f83030b615c457cc553cc
Emal Isuranga <emal.i@findmyfare.com>
2020-03-06 17:16:25 +0530

    register and table

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent }      from './login/login.component';
+import { RegisterComponent }      from './register/register.component';
+import { HomeComponent }      from './home/home.component';
+import { ManagerComponent }      from './manager/manager.component';
 
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent }
+  { path: '', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: HomeComponent },
+  { path: 'maneger', component: ManagerComponent },
 ];
 
 @NgModule({
